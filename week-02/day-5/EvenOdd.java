5c2322c497f5bb9759de02e72678cf80c9ffa53a
kovacskataGF <drkovacskata@mensa.hu>
2021-10-24 22:53:46 +0200

    Java feladatok - 2. heti anyag

---
+++ week-02/day-5/EvenOdd.java
@@ -0,0 +1,16 @@
+package conditionals;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Adj meg 1 számot:");
+        int userNumber = scanner.nextInt();
+        if (userNumber % 2 == 0) {
+            System.out.println("Páros");
+        } else {
+            System.out.println("Páratlan");
+        }
+    }
+}
---
+++ week-02/day-5/IWontCheatInTheExam.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class IWontCheatInTheExam {
+    public static void main(String[] args) {
+        // Írj egy programot ami 100-szor kiírja:
+        // "Nem fogok csalni a vizsgán!"
+        int i = 0;
+        while (i < 100) {
+            System.out.println("\"Nem fogok csalni a vizsgán!\"");
+            i++;
+        }
+    }
+}
